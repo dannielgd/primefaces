2a3e1085adee5f070b01efeca66314e55b7d9ab9
Danniel <Danniel@DESKTOP-G7ES9JE>
2019-06-03 12:40:06 -0300

    Spring Security com erro

--- PedidoVenda/src/main/java/com/algaworks/pedidovenda/repository/Usuarios.java
+++ PedidoVenda/src/main/java/com/algaworks/pedidovenda/repository/Usuarios.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceException;
 
 import org.apache.commons.lang3.StringUtils;
@@ -22,56 +23,66 @@ import com.algaworks.pedidovenda.util.jpa.Transactional;
 public class Usuarios implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Inject
 	private EntityManager manager;
 
 	public Usuario guardar(Usuario usuario) {
-		Usuario usr = manager.merge(usuario); //salva ou atualiza no bd
+		Usuario usr = manager.merge(usuario); // salva ou atualiza no bd
 		return guardarGrupo(usr);
 	}
-	
+
 	public Usuario guardarGrupo(Usuario usuario) {
 		return manager.merge(usuario);
 	}
-	
+
 	@Transactional
 	public void remover(Usuario usuario) {
-		
+
 		try {
-		usuario = porId(usuario.getId());
-		manager.remove(usuario); //marca para exclusão
-		manager.flush();
+			usuario = porId(usuario.getId());
+			manager.remove(usuario); // marca para exclusão
+			manager.flush();
 		} catch (PersistenceException e) {
 			throw new NegocioException("Usuário não pode ser excluído!");
 		}
 	}
-	
-	
 
 	public List<Usuario> filtrados(UsuarioFilter filtro) {
 		Session session = (Session) manager.unwrap(Session.class);
-		
+
 		Criteria criteria = session.createCriteria(Usuario.class);
-		
-		//where nome like '%joao%'
-		if(StringUtils.isNotBlank(filtro.getNome())) {
+
+		// where nome like '%joao%'
+		if (StringUtils.isNotBlank(filtro.getNome())) {
 			criteria.add(Restrictions.ilike("nome", filtro.getNome(), MatchMode.ANYWHERE));
 		}
-		
+
 		return criteria.addOrder(Order.asc("nome")).list();
 	}
 
 	public Usuario porId(Long id) {
 		Usuario usuario = manager.find(Usuario.class, id);
 		return usuario;
-		
+
 	}
-	
+
 	public List<Usuario> vendedores() {
-		//TODO filtrar apenas vendedores por grupo específico.
-		return this.manager.createQuery("from Usuario", Usuario.class)
-				.getResultList();
+		// filtrar apenas vendedores por grupo específico.
+		return this.manager.createQuery("from Usuario", Usuario.class).getResultList();
 	}
-	
+
+	public Usuario porEmail(String email) {
+		Usuario usuario = null;
+
+		try {
+			usuario = this.manager.createQuery("from Usuario where lower(email) = :email", Usuario.class)
+					.setParameter("email", email.toLowerCase()).getSingleResult();
+		} catch (NoResultException e) {
+			// nenhum usuário encontrado com o e-mail informado
+		}
+
+		return usuario;
+	}
+
 }

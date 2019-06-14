e06f8d41631e47ba665a80ae117b6f02dbf0d25a
Danniel <dannielgd@gmail.com>
2019-06-14 12:44:03 -0300

    Criando os métodos para popular o gráfico de pedidos - Aula 16.2

---
+++ PedidoVenda/src/main/java/TesteConsulta.java
@@ -0,0 +1,96 @@
+
+import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.apache.commons.lang3.time.DateUtils;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.transform.Transformers;
+import org.hibernate.type.StandardBasicTypes;
+import org.hibernate.type.Type;
+
+import com.algaworks.pedidovenda.model.Pedido;
+import com.algaworks.pedidovenda.model.Usuario;
+import com.algaworks.pedidovenda.model.vo.DataValor;
+
+public class TesteConsulta {
+
+	public static void main(String[] args) {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("PedidoPU"); 
+		EntityManager manager = factory.createEntityManager();
+		Session session = manager.unwrap(Session.class);
+		
+		Usuario usuario = new Usuario();
+		usuario.setId(1L);
+		
+		Map<Date, BigDecimal> valores = valoresTotaisPorData(15, usuario, session);
+		int count = 1;
+		for (Date data : valores.keySet()) {
+			System.out.println(count + " - " + data + " = " + valores.get(data));
+			count++;
+		}
+		
+		manager.close();
+
+	}
+
+	@SuppressWarnings({ "unchecked" })
+	public static Map<Date, BigDecimal> valoresTotaisPorData(Integer numeroDeDias, Usuario criadoPor, Session session) {
+
+		numeroDeDias -= 1;
+		Calendar dataInicial = Calendar.getInstance();
+		dataInicial = DateUtils.truncate(dataInicial, Calendar.DAY_OF_MONTH);
+
+		dataInicial.add(Calendar.DAY_OF_MONTH, numeroDeDias * -1);
+
+		Map<Date, BigDecimal> resultado = criarMapaVazio(numeroDeDias, dataInicial);
+		
+		Criteria criteria = session.createCriteria(Pedido.class);
+		
+		// select date(data_criacao) as data, sum(valor_total) as valor
+		// from pedido where data_criacao >= :dataInicial and vendedo_id = :criadoPor
+		// group by date(data_criacao)
+		criteria = criteria.setProjection(Projections.projectionList()
+				.add(Projections.sqlGroupProjection("date(data_criacao) as data", 
+						"date(data_criacao)", new String[] { "data" }, 
+						new Type[] { StandardBasicTypes.DATE } ))
+				.add(Projections.sum("valorTotal").as("valor"))
+				).add(Restrictions.ge("dataCriacao", dataInicial.getTime()));
+		
+		if(criadoPor != null) {
+			criteria.add(Restrictions.eq("vendedor", criadoPor));
+		}
+		
+		List<DataValor> valoresPorData = criteria.setResultTransformer(Transformers.aliasToBean(DataValor.class)).list();
+		
+		for(DataValor dataValor : valoresPorData) {
+			resultado.put(dataValor.getData(), dataValor.getValor());
+		}
+		
+		return resultado;
+	}
+
+	private static Map<Date, BigDecimal> criarMapaVazio(Integer numeroDeDias, Calendar dataInicial) {
+
+		Map<Date, BigDecimal> mapaInicial = new TreeMap<>();
+
+		dataInicial = (Calendar) dataInicial.clone();
+		for (int i = 0; i <= numeroDeDias; i++) {
+			mapaInicial.put(dataInicial.getTime(), BigDecimal.ZERO);
+			dataInicial.add(Calendar.DAY_OF_MONTH, 1);
+		}
+
+		return mapaInicial;
+	}
+
+}
